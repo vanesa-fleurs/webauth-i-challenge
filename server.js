947bacb974ca3fb28fb70e033c6eb8e625383c51
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-10-15 17:03:58 -0500

    complete MVP for day II

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const helmet = require('helmet')
-
+const sessions = require('express-session')
+// const knexSessionStore = require('connect-session-knex')(sessions)
+const knexConfig = require('./data/db.js')
 
 const server = express();
 
-server.use(express.json())
-server.use(helmet())
+
+const sessionConfig = {
+    name: 'node life',
+    secret: 'lalal', //shouldn't be hardcoded in
+    cookie: {
+        maxAge: 60 * 60 * 1000,
+        httpOnly: true, // the browser can't access via JS. always true
+        secure: false,
+    },
+    resave: false, //so expried sessions can't be re-saved 
+    saveUninitialized: false, //due to law
+//     store: new knexSessionStore({
+//         knex: knexConfig, 
+//         //weather you wnat to create a table for the sessions or not:
+//         //auto create sessions table in the database
+//         tablename: 'sessions',
+//         createtable: true,
+//         //clear interval for createtable
+//         clearInterval: 1000 * 60 * 60 //dleete expired sessions every 30 min. 
+
+//   })
+}
+
+server.use(express.json());
+server.use(helmet());
+server.use(sessions(sessionConfig));
 
 
 const userRouter = require('./users/users-router.js')
--- users/users-router.js
+++ users/users-router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const Users = require('./users-model.js');
 const bcrypt = require('bcryptjs')
-
+const restricted = require('../auth/restricted-middleware.js')
 // router.get('/', (req,res) => {
 //     Users.get()
 //     .then(users => {
@@ -18,12 +18,16 @@ router.post('/register', (req,res) => {
     let user = req.body;
     console.log("password from client -->", user.password)
     //overwrite password that user types
-    user.password = bcrypt.hashSync(user.password, 10)
-    console.log("password after hash & --> DB", user.password)
+    const hash = bcrypt.hashSync(user.password, 10) 
+    user.password = hash;
+    console.log("password after hash & --> DB", hash)
 
     Users.add(user)
         .then(storeUser => {
-            res.status(200).json(storeUser)
+            //add the uer to the session. Now meta-info. about the user is attatched
+            //to the session! 
+            // req.session.user = saved;...
+            res.status(201).json(storeUser)
         })
         .catch(error => {
             console.log(error)
@@ -39,7 +43,11 @@ router.post('/login', (req,res) => {
         .then(userExists => {
             //if user exists & password matches
             if(userExists && bcrypt.compareSync(password, userExists.password)){
-                res.status(200).json({messsage: `Hello, ${userExists.username}`})
+                //add the uer to the session. Now meta-info. about the user is attatched
+                //to the session! 
+                req.session.username = userExists.username;
+                //cookie is automatically sent back! 
+                res.status(200).json({messsage: `Hello, ${userExists.username}, Logged In.`})
 
             }
             else{
@@ -52,39 +60,43 @@ router.post('/login', (req,res) => {
         })
 })
 
-router.get('/', restrict, (req,res) => {
+router.get('/', restricted, (req,res) => {
+    console.log('username in GET /', req.session.username);
     Users.find()
         .then(allUsers => {
             res.status(200).json(allUsers)
         })
         .catch(error => {
             console.log(error)
-            res.status(500).json({error: `error logging in.`})
+            res.status(500).json({error: `error logging in. You shall not pass!`})
         })
  
 })
 
 //Middleware
-function restrict(req,res, next) {
-    const { username, password } = req.headers
-    if( username && password){
-        Users.findBy({username})
-            .then(userExists =>{
-                if(userExists && bcrypt.compareSync(password, userExists.password)){
-                    next();
-                } 
-                else{
-                    res.status(401).json({message: `invalid credentials`})
-                }
-            })
-            .catch(error => {
-                console.log(error)
-                res.status(500).json({error: `error`})
-            })
-    } 
-    else{
-        res.status(401).json({message: `provide username & password`})
-    }
-}
+// function restrict(req,res, next) {
+//     const { username, password } = req.headers
+//     if( username && password){
+//         Users.findBy({username})
+//             .then(userExists =>{
+//                 if(userExists && bcrypt.compareSync(password, userExists.password)){
+//                     next();
+//                 } 
+//                 else{
+//                     res.status(401).json({message: `invalid credentials`})
+//                 }
+//             })
+//             .catch(error => {
+//                 console.log(error)
+//                 res.status(500).json({error: `error`})
+//             })
+//     } 
+//     else{
+//         res.status(401).json({message: `provide username & password`})
+//     }
+// }
 
+function restrict(req,res,next) {
+    
+}
 module.exports = router
